7367fb06030cf50f98815515c554d8c66c2d9833
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-01-29 19:20:39 +0300

    программа работает корректно и исправно

---
+++ DomZadanie№6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("введите ваше число товарищ: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+  Console.Write("цифра чётная и делится на 2");
+}
+else 
+{
+  Console.Write("цифра не чётная ");
+}
