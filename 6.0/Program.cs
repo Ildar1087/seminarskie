5731b337f999286129ce117b6c4d347e3192e1e7
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-01-29 18:49:43 +0300

    2 variant - ne rabotaet

---
+++ 6.0/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 || num > 999)
+Console.Write("введено не корректное значение");
+return;
+///int otvet = num% 10;
+///Console.WriteLine($"3 цифра"{otvet});
+string number = Console.ReadLine();
+string otvet2 = Convert.ToString(number[2]);
+Console.WriteLine($"3 цифра {otvet2}");
+
