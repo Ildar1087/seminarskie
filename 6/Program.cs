5a18665c406a62cdb0ddc9f9d2646c2c0b9c5944
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-01-29 18:33:09 +0300

    new folder 6

---
+++ 6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 || num > 999)
+Console.Write("введено не корректное значение");
+return;
+int otvet = num% 10;
+Console.WriteLine($"3 цифра" {otvet});
