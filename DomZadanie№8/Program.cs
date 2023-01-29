5bb5bb84b2867ec4e1952124bdf52861a48706a5
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-01-29 21:00:28 +0300

    написал программу и она работает как надо

---
+++ DomZadanie№8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("введите ваше число товарищ: ");
+int num = int.Parse(Console.ReadLine());
+int a1 = 0;
+while (a1 < num)
+{
+  a1 = a1 + 2;
+   Console.Write($"{a1}");
+}
